b335ad08c73a9784cbb5083b3b80183a46cf3845
Elijahscriptdev <elijahobominuru@gmail.com>
2021-04-18 19:16:50 +0100

    project setup

---
+++ src/navigation/topNav/TopNav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./TopNav.css";
+
+const TopNav = () => {
+  return (
+    <div className="top-nav">
+      <div>
+        <ul>
+          <li>logo</li>
+        </ul>
+      </div>
+      <div className="top-nav-links">
+        <ul>
+          <li className="">one</li>
+          <li>two</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
